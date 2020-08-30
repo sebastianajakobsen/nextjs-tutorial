f5ca9c8d5d3e787bbd2417801396c7be130b4848
faken09 <seb_jakobsen@live.dk>
2020-08-30 12:55:31 +0200

    more testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from "next/link";
 import {Post} from "./api/post";
+import {GetServerSideProps} from "next";
 
 
 export interface Props {
@@ -25,18 +26,12 @@ const Index:React.FC<Props> = ({posts}) => {
     );
 };
 
-export async function getStaticProps() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+export const getServerSideProps: GetServerSideProps = async () => {
+    // Fetch data from external API
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
     const posts = await res.json()
-    return {
-        props: {
-            posts,
-        },
-        // Next.js will attempt to re-generate the page:
-        // - When a request comes in
-        // - At most once every second
-        revalidate: 10, // In seconds
-    }
+    // Pass data to the page via props
+    return { props: { posts } }
 }
 
 
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import {Post} from "../api/post";
+import {GetServerSideProps} from "next";
 
 interface Props {
-
+    post:Post
 }
 
-const Id: React.FC<Props> = ({}) => {
+const Id: React.FC<Props> = ({post}) => {
     return (
         <div>
-            Hello
+            {post.title}
         </div>
     );
 };
+
+// This gets called on every request
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    // Fetch data from external API
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.query.id}`)
+    const post = await res.json()
+    // Pass data to the page via props
+    return { props: { post } }
+}
+
+
 
 export default Id
